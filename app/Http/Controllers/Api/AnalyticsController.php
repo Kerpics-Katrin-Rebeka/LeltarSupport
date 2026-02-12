6153c76e3a56edcedcb9078b1fd34f75a7d7a9a1
Kerpics-Katrin-Rebeka <kerpics.katrin.rebeka@students.jedlik.eu>
2026-02-12 11:32:10 +0100

    analytics suggestion

--- app/Http/Controllers/Api/AnalyticsController.php
+++ app/Http/Controllers/Api/AnalyticsController.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Inventory;
 
 class AnalyticsController extends Controller
 {
-    //
+    public function reorderSuggestion()
+    {
+        $lowStock = Inventory::with('ingredient')
+            ->whereColumn('quantity','<','minimum_level')
+            ->get();
+
+        return response()->json($lowStock);
+    }
 }
