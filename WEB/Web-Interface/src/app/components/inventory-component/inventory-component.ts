1a1ef8f657cce6b233b7b57cd38188c3139109e7
feher-kristof1 <feher.kristof1@students.jedlik.eu>
2026-03-02 14:43:40 +0100

    inventory table started

--- WEB/Web-Interface/src/app/components/inventory-component/inventory-component.ts
+++ WEB/Web-Interface/src/app/components/inventory-component/inventory-component.ts
@@ -9,6 +9,21 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class InventoryComponent {
   @Output() backToMenu = new EventEmitter;
   backButton:string = "<-"
+  goal:number=100;
+
+  ngOnInit(){
+    var fullers = document.getElementsByClassName("fuller") as  HTMLCollectionOf<HTMLDivElement>;
+    var bars  = document.getElementsByClassName("bar") as  HTMLCollectionOf<HTMLDivElement>;
+    for (let i = 0; i < bars.length; i++) {
+      var curr = this.rng();
+      fullers[i].style.height = `${(curr/this.goal)*100}%`;
+      bars[i].appendChild(fullers[i])
+    }
+  }
+
+  rng(){
+    return Math.round(Math.random()*100)
+  }
 
   goBack(){
     this.backToMenu.emit("menu");
